64005186a5ba73fefe957f98df75ec70086c9a0f
whatsuplinlin <kjm010704@gmail.com>
2022-08-04 14:40:45 +0700

    1.6

--- src/views/event/EventEditView.vue
+++ src/views/event/EventEditView.vue
@@ -1,9 +1,31 @@
 <template>
   <p>Edit the event here</p>
+  <button @click="register">Edit Me</button>
 </template>
 
 <script>
 export default {
-  props: ['id', 'event']
+  props: ['id', 'event'],
+  inject: ['GStore'],
+  methods: {
+    register() {
+      // Assuming successful API call to register them
+
+      // Set a flash message to appear on the next page loaded which says
+      // 'You are successfully registered for ' + this.event.title
+
+      this.GStore.flashMessage =
+        'The data has been updated for ' + this.event.title
+      setTimeout(() => {
+        //After 3 seconds remove it
+        this.GStore.flashMessage = ''
+      }, 3000)
+
+      this.$router.push({
+        name: 'EventDetails',
+        params: { id: this.event.id }
+      })
+    }
+  }
 }
 </script>
